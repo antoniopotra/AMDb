7f73a240c1a5d6b4449c8a4123393b266e1143e2
antoniopotra <antonio.potra@gmail.com>
2022-12-13 00:16:19 +0200

    homepage

---
+++ public/js/homepage.js
@@ -0,0 +1,9 @@
+function openForm(id) {
+    document.getElementById(id).style.display = "block";
+    document.getElementById("container").style.opacity = "0.2";
+}
+
+function closeForm(id) {
+    document.getElementById(id).style.display = "none";
+    document.getElementById("container").style.opacity = "1";
+}
