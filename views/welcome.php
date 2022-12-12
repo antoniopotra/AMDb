93e501e31a81881d7b5a3e2be442c7b49910208e
antoniopotra <antonio.potra@gmail.com>
2022-12-13 00:20:46 +0200

    renamed files

--- views/welcome.php
+++ views/welcome.php
@@ -7,14 +7,14 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>AMDb</title>
-    <link href="../public/css/homepage.css" rel="stylesheet" type="text/css">
-    <script src="../public/js/homepage.js"></script>
+    <link href="../public/css/welcome.css" rel="stylesheet" type="text/css">
+    <script src="../public/js/welcome.js"></script>
     <script src="https://kit.fontawesome.com/a20944aa28.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
     <div class="container" id="container">
-        <img src="../public/images/homepage-background.jpg" alt="">
+        <img src="../public/images/welcome-background.jpg" alt="">
 
         <div class="inner-container">
             <div class="text-container">
