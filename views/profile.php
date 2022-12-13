97aead847936d79dd5d12a8acc2cc5ce96c68a2b
antoniopotra <antonio.potra@gmail.com>
2022-12-13 16:56:55 +0200

    started profile

---
+++ views/profile.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Profile</title>
+    <link href="../public/css/profiler.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+    <div class="menu-wrapper">
+        <ul class="menu">
+            <li class="menu-item">LOGO</li>
+            <li class="menu-item">MOVIES</li>
+            <li class="menu-item">ACCOUNT</li>
+            <li class="menu-item">SEARCH</li>
+        </ul>
+    </div>
+</body>
+</html>
