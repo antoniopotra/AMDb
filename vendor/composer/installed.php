490505001b4133e6b438a662d111f510ce898a5c
antoniopotra <antonio.potra@gmail.com>
2023-02-02 12:23:00 +0200

    heroku preparation

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor_name/amdb',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'defe8f8c59fb365679fc692d55a5c2850c88a778',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/amdb' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'defe8f8c59fb365679fc692d55a5c2850c88a778',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
